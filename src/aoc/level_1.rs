ad95fdeee0655f3c716baceb5dc6f0531250a090
Giuseppe D'Amico <gd.mc@icloud.com>
2022-12-01 23:44:32 +0100

    some much needed refactor for level 1

--- src/aoc/level_1.rs
+++ src/aoc/level_1.rs
@@ -1,40 +1,26 @@
 use crate::helpers;
 
-fn get_all_calories(elfs: &str) -> impl Iterator<Item=i32> + '_ {
-    elfs
-        .split("\n\n")
+pub fn solve() -> (i32, i32) {
+    let elfs_input = helpers::load_input::select(1).unwrap();
+    let mut elfs_kcal = elfs_input.split("\n\n")
         .map(|every_kcal| {
             every_kcal
-                .split('\n')
+                .split('\n') // found out that this split can be replaced by split_whitespace()
+                             // not going to use it as it was not part of the original solution.
+                             // still learned something new :D
                 .fold(0, |acc, kcal| {
                     kcal.parse::<i32>().unwrap_or(0) + acc
-                })
+                }) 
         })
-}
-
-pub fn level01_part1() -> i32 {
-    let res = helpers::load_input::select(1).unwrap();
-    let elfs_kcal = get_all_calories(&res);
-
-    elfs_kcal
-        .reduce(|acc, kcal| {
-            if kcal > acc { kcal } else { acc }
-        })
-        .unwrap()
-}
-
-pub fn level01_part2() -> i32 {
-    let res = helpers::load_input::select(1).unwrap();
-    let mut elfs_kcal = get_all_calories(&res).collect::<Vec<i32>>();
+        .collect::<Vec<i32>>();
 
     // AAAAAHHHHHHH, MUTABILITYYYYYYYYYY!!! THE HORROR!!!
     elfs_kcal.sort();
     elfs_kcal.reverse();
 
-    elfs_kcal
-        .get(0..2)
-        .unwrap()
-        .iter()
-        .fold(0, |acc, kcal| acc + kcal) // can be refactor with a reduce, but I don't have the
-                                          // time to fight with the borrow checker right now.
+    (
+        elfs_kcal.get(0).unwrap().to_owned(),
+        elfs_kcal.get(0..3).unwrap().iter().sum()
+    )
 }
+
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ extern crate reqwest;
 mod helpers;
 mod aoc;
 
-use crate::aoc::level_1::*;
+use crate::aoc::*;
  
 fn main() {
-    println!("Welcome to Advent of Code 2022");
-    println!("AoC 1 - 1, result: {}", level01_part1());
-    println!("AoC 1 - 2, result: {}", level01_part2());
+    println!("Welcome to Advent of Code 2022!");
+
+    let (level_1_1, level_1_2) = level_1::solve();
+    println!("AoC 1 - 1, result: {}", level_1_1);
+    println!("AoC 1 - 2, result: {}", level_1_2);
 }
