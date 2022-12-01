a8e50381210e74f76c71dd316bee9c172c8c5862
Giuseppe D'Amico <gd.mc@icloud.com>
2022-12-01 22:18:44 +0100

    stupid me left some async boilerplate everywhere

--- src/aoc/level_1.rs
+++ src/aoc/level_1.rs
@@ -12,20 +12,18 @@ fn get_all_calories(elfs: &str) -> impl Iterator<Item=i32> + '_ {
         })
 }
 
-pub async fn level01_part1() -> Result<String, ()> {
+pub fn level01_part1() -> i32 {
     let res = helpers::load_input::select(1).unwrap();
     let elfs_kcal = get_all_calories(&res);
 
-    let split_elfs = elfs_kcal
+    elfs_kcal
         .reduce(|acc, kcal| {
             if kcal > acc { kcal } else { acc }
         })
-        .unwrap();
-
-    Ok(split_elfs.to_string())
+        .unwrap()
 }
 
-pub async fn level01_part2() -> Result<String, ()> {
+pub fn level01_part2() -> i32 {
     let res = helpers::load_input::select(1).unwrap();
     let mut elfs_kcal = get_all_calories(&res).collect::<Vec<i32>>();
 
@@ -33,12 +31,10 @@ pub async fn level01_part2() -> Result<String, ()> {
     elfs_kcal.sort();
     elfs_kcal.reverse();
 
-    let split_elfs = elfs_kcal
+    elfs_kcal
         .get(0..2)
         .unwrap()
         .iter()
-        .fold(0, |acc, kcal| acc + kcal); // can be refactor with a reduce, but I don't have the
+        .fold(0, |acc, kcal| acc + kcal) // can be refactor with a reduce, but I don't have the
                                           // time to fight with the borrow checker right now.
-
-    Ok(split_elfs.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ mod helpers;
 mod aoc;
 
 use crate::aoc::level_1::*;
-    
-
-#[tokio::main]
-async fn main() {
+ 
+fn main() {
     println!("Welcome to Advent of Code 2022");
-    println!("AoC 1 - 1, result: {}", level01_part1().await.unwrap());
-    println!("AoC 1 - 2, result: {}", level01_part2().await.unwrap());
+    println!("AoC 1 - 1, result: {}", level01_part1());
+    println!("AoC 1 - 2, result: {}", level01_part2());
 }
