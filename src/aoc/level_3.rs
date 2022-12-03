def29c49b9cfc0c306bb214ed5abe87bc8f20fb0
Giuseppe D'Amico <gd.mc@icloud.com>
2022-12-03 19:35:58 +0100

    playing around

--- src/aoc/level_3.rs
+++ src/aoc/level_3.rs
@@ -3,16 +3,16 @@ use crate::helpers::load_input;
 pub fn solve() -> (i32, i32) {
     let priorities = ('a'..='z').chain('A'..='Z').into_iter().collect::<Vec<char>>();
     let level = load_input::select(3).unwrap();
-    let lines_part_1 = level.lines();
-    let lines_vec = lines_part_1.clone().map(|l| l.to_string()).collect::<Vec<String>>();
-    let lines_part_2 = lines_vec.array_chunks::<3>();
+    let lines = Box::new(level.lines().map(|l| l.to_string()));
 
     fn get_index(i: &[char], search: &char) -> i32 {
         i.iter().position(|&r| r == *search).unwrap() as i32
     }
 
-    let get_single_sum = |source: &[char], comp: &[String]| {
+    let get_single_sum = |source: String, comp: &[String]| {
         let t = source
+            .chars()
+            .collect::<Vec<char>>()
             .iter()
             .fold(vec![], |mut acc, item| {
                 let filter_me_daddy = comp.iter().filter(|s| s.contains(*item));
@@ -28,21 +28,22 @@ pub fn solve() -> (i32, i32) {
     };
 
     let part_1: i32 = 
-        lines_part_1.map(|sack| {
-            let items_in_sack = sack.chars().collect::<Vec<char>>();
-            let half_len = items_in_sack.len() / 2;
-            let (comp1, comp2) = items_in_sack.split_at(half_len);
+        lines.clone()
+            .map(|sack| {
+                let half_len = sack.len() / 2;
+                let (comp1, comp2) = sack.split_at(half_len);
 
-            get_single_sum(comp1, &[comp2.iter().collect()])
-        }).sum();
+                get_single_sum(comp1.to_string(), &[comp2.to_string()])
+            }).sum();
 
     let part_2: i32 = 
-        lines_part_2.map(|group| {
-            let head = &group.first().unwrap().chars().collect::<Vec<char>>();
-            let tail = &group[1..];
+        lines.array_chunks::<3>()
+            .map(|group| {
+                let head = group.first().unwrap().to_owned();
+                let tail = &group[1..];
         
-            get_single_sum(head, tail)
-        }).sum();
+                get_single_sum(head, tail)
+            }).sum();
 
 
     (part_1, part_2)
